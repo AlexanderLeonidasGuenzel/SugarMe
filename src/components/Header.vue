89387b03b45cd6cad38a54a2d5fbdc107aed312a
Alexander Guenzel <alexanderleonidasguenzel@gmail.com>
2024-07-17 07:37:20 +0200

    add: header icon

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,7 +5,10 @@
 
 <template>
   <div class="flex flex-col gap-2 text-center mb-10 w-full" >
-    <h1 class="text-4xl font-bold text-cyan-700 bg-cyan-100 pt-2 pb-2 rounded-lg shadow-lg">Sugar Me!</h1>
+    <h1 class="flex justify-center text-4xl font-bold text-cyan-700 bg-cyan-100 pt-2 pb-2 pl-10 rounded-lg shadow-lg">
+      <img src="~@/assets/lollipop_10947115.png" alt="blue lollipop icon" class="w-10 mr-5">
+      Sugar Me!
+    </h1>
     <img src="@/assets/sugar.jpg" alt="sugar image" class="w-full h-40 object-cover rounded-lg shadow-lg">
   </div>
 </template>
