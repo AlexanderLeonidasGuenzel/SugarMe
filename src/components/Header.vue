e0db4ef8d233aaa41f6ac8a474f6859c90b621b8
Alexander Guenzel <alexanderleonidasguenzel@gmail.com>
2024-07-17 06:58:31 +0200

    change: sugar image smaller

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,6 +6,6 @@
 <template>
   <div class="flex flex-col gap-2 text-center mb-10 w-full" >
     <h1 class="text-4xl font-bold text-cyan-700 bg-cyan-100 pt-2 pb-2 rounded-lg shadow-lg">Sugar Me!</h1>
-    <img src="@/assets/sugar.jpg" alt="sugar image" class="w-full h-60 object-cover rounded-lg shadow-lg">
+    <img src="@/assets/sugar.jpg" alt="sugar image" class="w-full h-40 object-cover rounded-lg shadow-lg">
   </div>
 </template>
