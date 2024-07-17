da554ff2d5a17bf99f897e965840f0b494b78ed7
Alexander Guenzel <alexanderleonidasguenzel@gmail.com>
2024-07-17 17:51:37 +0200

    change: footer position

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,11 +3,14 @@
 </script>
 
 <template>
-
-    <div class="flex flex-col items-center text-center w-full mb-5">
-      <p class="self-end justify-self-center mr-5 mb-2">Powered by <a href="https://world.openfoodfacts.org/" target="_blank"><span class="italic font-bold">Open Food Facts API</span></a></p>
-      <img src="@/assets/footer-sugar.jpg" alt="sugar image" class="w-full h-24 object-cover rounded-lg shadow-lg border-white border-3">
+    <div class="h-1/3 w-full flex flex-col justify-end mt-10">
+      <div class="flex w-full"></div>
+      <div class="flex flex-col items-center text-center w-full mb-5 h-32">
+        <p class="self-end justify-self-center mr-5 mb-2">Powered by <a href="https://world.openfoodfacts.org/" target="_blank"><span class="italic font-bold">Open Food Facts API</span></a></p>
+        <img src="@/assets/footer-sugar.jpg" alt="sugar image" class="w-full h-24 object-cover rounded-lg shadow-lg border-white border-3">
+      </div>
     </div>
+
 
 
 </template>
