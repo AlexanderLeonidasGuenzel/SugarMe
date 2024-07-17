12653bfe7b323176aecd01f36c729d3769030ec0
Alexander Guenzel <alexanderleonidasguenzel@gmail.com>
2024-07-17 07:51:28 +0200

    change: header design fixed

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,8 +5,12 @@
 
 <template>
   <div class="flex flex-col gap-2 text-center mb-10 w-full" >
-    <h1 class="flex justify-center text-4xl font-bold text-cyan-700 bg-cyan-100 pt-2 pb-2 pl-10 rounded-lg shadow-lg">
-      <img src="~@/assets/lollipop_10947115.png" alt="blue lollipop icon" class="w-10 mr-5">
+    <div>
+
+    </div>
+
+    <h1 class="flex justify-center text-4xl font-bold text-cyan-700 bg-cyan-100 py-2.5 rounded-lg shadow-lg">
+      <img src="~@/assets/lollipop_10947115.png" alt="blue lollipop icon" class="w-10 mr-3">
       Sugar Me!
     </h1>
     <img src="@/assets/sugar.jpg" alt="sugar image" class="w-full h-40 object-cover rounded-lg shadow-lg">
