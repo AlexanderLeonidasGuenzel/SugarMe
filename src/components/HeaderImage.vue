0989f10e26acd4bc42bc7103249a9efa03d831ec
Alexander Guenzel <alexanderleonidasguenzel@gmail.com>
2024-07-17 04:40:39 +0200

    change: header design

--- src/components/HeaderImage.vue
+++ src/components/HeaderImage.vue
@@ -5,7 +5,7 @@
 
 <template>
   <div class="flex flex-col gap-2 text-center mb-10 w-full" >
-    <h1 class="text-3xl font-bold mr-6 text-cyan-700 z-10">Sugar Me!</h1>
-    <img src="@/assets/sugar.jpg" alt="sugar image" class="w-full object-cover rounded-lg shadow-lg z-0">
+    <h1 class="text-4xl font-bold text-cyan-700 bg-cyan-100 pt-2 pb-2 rounded-lg shadow-lg">Sugar Me!</h1>
+    <img src="@/assets/sugar.jpg" alt="sugar image" class="w-full h-60 object-cover rounded-lg shadow-lg">
   </div>
 </template>
