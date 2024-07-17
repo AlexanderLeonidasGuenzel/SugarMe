4ebba9c7ecaa620478e100da7cff9d71cbdc09b0
Alexander Guenzel <alexanderleonidasguenzel@gmail.com>
2024-07-17 04:22:12 +0200

    change: design

--- src/components/Headercomponent.vue
+++ src/components/Headercomponent.vue
@@ -4,7 +4,7 @@
 </script>
 
 <template>
-  <div class="flex justify-center items-center w-full">
+  <div class="flex justify-center items-center w-full mb-10">
     <input type="text" class="w-3/4 h-10 outline-none rounded mr-2 px-1">
     <button class="flex justify-center items-center bg-cyan-700 text-white text-md w-10 h-10 rounded-md mr-2"><span class="searchicon">search</span></button>
     <button class="flex justify-center items-center bg-cyan-700 text-white text-md w-10 h-10 rounded-md"><span class="barcodeicon">barcode_scanner</span></button>
